ed10ba06f75b6f94374a96e332ed77d70a14b9be
fdsonzogni <fdsonzogni@gmail.com>
2024-03-29 15:24:06 +0000

    auto commit 1       20240329 152405

--- src/rpart/z122_traintest_montecarlo.r
+++ src/rpart/z122_traintest_montecarlo.r
@@ -7,7 +7,7 @@ require("parallel")
 
 PARAM <- list()
 # reemplazar por las propias semillas
-PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
+PARAM$semillas <- c(123401, 202127, 966439, 155219, 248879)
 
 #------------------------------------------------------------------------------
 # particionar agrega una columna llamada fold a un dataset que consiste
