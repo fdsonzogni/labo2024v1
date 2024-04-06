1504f5c495dbf8a44a9c0a95944e3fae3edd5a74
fdsonzogni <fdsonzogni@gmail.com>
2024-04-06 14:48:44 +0000

    1erModelo_Ejec

--- src/rpart/fds_z101_PrimerModelo_Enclase3.R
+++ src/rpart/fds_z101_PrimerModelo_Enclase3.R
@@ -21,10 +21,10 @@ modelo <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
-        cp = -0.5, # esto significa no limitar la complejidad de los splits
-        minsplit = 1340, # minima cantidad de registros para que se haga el split
-        minbucket = 600, # tamaño minimo de una hoja
-        maxdepth = 6
+        cp = -0.9, # esto significa no limitar la complejidad de los splits
+        minsplit = 800, # minima cantidad de registros para que se haga el split
+        minbucket = 240, # tamaño minimo de una hoja
+        maxdepth = 14
 ) # profundidad maxima del arbol
 
 
@@ -60,6 +60,6 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_001.csv",
+        file = "./exp/KA2001/K101_clase3_001.csv",
         sep = ","
 )
