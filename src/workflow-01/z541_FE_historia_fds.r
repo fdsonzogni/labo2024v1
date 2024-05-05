bbbf856f51d38407ae6d536fddb19e7b0fe84cda
fdsonzogni <fdsonzogni@gmail.com>
2024-05-05 03:20:14 +0000

    FE_error_please

--- src/workflow-01/z541_FE_historia_fds.r
+++ src/workflow-01/z541_FE_historia_fds.r
@@ -231,14 +231,14 @@ AgregaVarRandomForest <- function(
     }
     }
 
-    # Obtener las columnas con valores faltantes
-      missing_columns <- colnames(dataset_rf)[colSums(is.na(dataset_rf)) > 0]
-    
-    # Verificar qué columnas tienen todos los valores faltantes
-      all_missing_columns <- missing_columns[colSums(is.na(dataset_rf[, missing_columns])) == nrow(dataset_rf)]
+  # Obtener las columnas con valores faltantes
+    missing_columns <- colnames(dataset_rf)[colSums(is.na(dataset_rf)) > 0]
 
-    # Imputar NA en las columnas con todos los valores faltantes
-      dataset_rf[, (all_missing_columns) := lapply(.SD, function(x) if(all(is.na(x))) NA else x), .SDcols = all_missing_columns]
+  # Verificar qué columnas tienen todos los valores faltantes
+    all_missing_columns <- missing_columns[colSums(is.na(dataset_rf[, missing_columns])) == nrow(dataset_rf)]
+
+  # Imputar NA en las columnas con todos los valores faltantes
+    dataset_rf[, (all_missing_columns) := lapply(.SD, function(x) if(all(is.na(x))) NA else x), .SDcols = ..all_missing_columns]
 
   # imputo los nulos, ya que ranger no acepta nulos
   # Leo Breiman, ¿por que le temias a los nulos?
